fec95639bb2b70aa59253453863cf2ed4929da0c
Kieran Brown <kswb96@gmail.com>
2024-02-14 22:05:35 +0000

    "Update the package list"

--- etc/setup-darwin.sh
+++ etc/setup-darwin.sh
@@ -5,4 +5,8 @@ if [ "$(uname)" != "Darwin" ]; then
   echo "Operating system is not Darwin"; exit 1
 fi
 
-xcode-select --install
+xcode-select --install &> /dev/null
+
+until xcode-select --print-path &> /dev/null; do
+  sleep 5;
+done
