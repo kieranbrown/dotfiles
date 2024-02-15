9ce1e17de8db14e70d936be4a67870be4cd3ca50
Kieran Brown <kswb96@gmail.com>
2024-02-15 00:08:34 +0000

    "Update the package list"

--- etc/install.sh
+++ etc/install.sh
@@ -35,6 +35,7 @@ if [[ $(command -v brew) == "" ]]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
   test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  test -d /opt/homebrew && eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 HOMEBREW_PREFIX="$(brew --prefix)"
